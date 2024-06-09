37b9158892ec5c3fd6002e59470e4101bbc56c11
memonadil02 <memonadil970@gmail.com>
2024-06-09 17:07:35 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-const parent = React.createElement("div", { id: "parent"}, [
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child", xyz: "adil memon" }, [
-    React.createElement("h1", {}, "i am an h1"),
+    React.createElement("h1", {}, "namste react"),
     React.createElement("h2", {}, "i am an h2"),
   ]),
   React.createElement("div", { id: "child2", xyz: "adil memon" }, [
-    React.createElement("h1", {}, "i am an h1"),
-    React.createElement("h2", {}, "i am an h2"),
+    React.createElement("h1", {}, "kya bolte pasha "),
+    React.createElement("h2", {}, "aaj kamayenga to kal khayenga aa meri jaan"),
   ]),
 ]);
 
